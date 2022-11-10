95fcda69ed80847a75983475b18c2f89860afab2
Michelly Nonato <smartnovo01@gmail.com>
2022-11-10 16:57:22 -0300

    JavaScript - explorando a linguagem

---
+++ One Oracle Next Edition/JavaScript a linguagem da Web/JavaScript - explorando a linguagem/aula 2/2-variaveis.js
@@ -0,0 +1,7 @@
+console.log("Trabalhando com variáveis");
+
+const idade = 31;
+const nome = "Ricardo";
+let ano = "2022"
+
+console.log("Meu nome é "+nome+" e tenho "+idade+" anos.");
---
+++ One Oracle Next Edition/JavaScript a linguagem da Web/JavaScript - explorando a linguagem/aula 2/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com atribuções de variáveis");
+
+const idade = 19;
+const primeiroNome = "Michelly";
+
+let sobreNome = "Nonato";
+let nomeCompleto = primeiroNome + sobreNome;
+
+console.log(primeiroNome, sobreNome);
+
+console.log(`Meu nome é ${primeiroNome} ${sobreNome}`);
+console.log(`Meu nome é ${nomeCompleto}`);
