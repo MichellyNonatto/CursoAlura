899361daa8e6ef6c9cec0bec6d2e56d8eae9a35a
Michelly Nonato <smartnovo01@gmail.com>
2022-11-19 15:07:48 -0300

    JavaScript - manipulação do DOM

--- One Oracle Next Edition/JavaScript a linguagem da Web/JavaScript - manipulação do DOM/assets/js/listaDeTarefa.js
+++ One Oracle Next Edition/JavaScript a linguagem da Web/JavaScript - manipulação do DOM/assets/js/listaDeTarefa.js
@@ -1,21 +1,37 @@
-const criarTarefa = (evento) =>{
-    evento.preventDefault();
+(() => {
+    const criarTarefa = (evento) => {
+        evento.preventDefault();
 
-    const input = document.querySelector("[data-form-input]");
-    const valorImput = input.value;
-    const lista = document.querySelector("[data-list]");
-    const tarefa = document.createElement('li');
-    tarefa.classList.add('task');
-    const conteudo = `<p class =content>${valorImput}</p>`;
+        const input = document.querySelector("[data-form-input]");
+        const valorImput = input.value;
+        const lista = document.querySelector("[data-list]");
+        const tarefa = document.createElement('li');
+        tarefa.classList.add('task');
+        const conteudo = `<p class =content>${valorImput}</p>`;
 
-    tarefa.innerHTML = conteudo;
+        tarefa.innerHTML = conteudo;
 
-    lista.appendChild(tarefa);
-    input.value = " ";
-}
+        tarefa.appendChild(concluir());
+        lista.appendChild(tarefa);
+        input.value = " ";
+    }
 
+    const novaTarefa = document.querySelector("[data-form-button]");
+    novaTarefa.addEventListener('click', criarTarefa);
 
-const novaTarefa = document.querySelector("[data-form-button]");
+    const concluir = () => {
+        const botaoConcluir = document.createElement('butto');
 
+        botaoConcluir.classList.add('fa-solid', 'fa-check', 'check-button');
 
-novaTarefa.addEventListener('click', criarTarefa);
+        botaoConcluir.addEventListener('click', concluirTarefa);
+
+        return botaoConcluir
+    }
+
+    const concluirTarefa = (evento) => {
+        const botaoConcluir = evento.target;
+        const tarefaCompleta = botaoConcluir.parentElement;
+        tarefaCompleta.classList.toggle('done');
+    }
+})();
