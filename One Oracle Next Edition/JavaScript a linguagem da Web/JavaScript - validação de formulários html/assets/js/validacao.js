c95a656bb87c9593977c1c0684aba6ec9dbd97cc
Michelly Nonato <smartnovo01@gmail.com>
2023-01-04 14:52:30 -0300

    aula 4

--- One Oracle Next Edition/JavaScript a linguagem da Web/JavaScript - validação de formulários html/assets/js/validacao.js
+++ One Oracle Next Edition/JavaScript a linguagem da Web/JavaScript - validação de formulários html/assets/js/validacao.js
@@ -36,12 +36,17 @@ const menssagensDeErro = {
     dataNascimento: {
         valueMissing: 'O campo data nascimento não pode estar vazio.',
         customError: 'Você deve ser maior de 18 anos para se cadastrar.'
+    },
+    cpf:{
+        valueMissing: 'O campo CPF não pode estar vazio.',
+        customError: 'O CPF está inválido.'
     }
    
 }
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input)
 }
 
 function mostrarMessagemDeErro(tipoDeInput, input){
@@ -71,3 +76,67 @@ function maiorQue18(data) {
     return dataMais18 <= dataAtual
 }
 
+function validaCPF(input){
+    const cpfFormatado = input.value.replace(/\D/g, '')
+    let mensagem = ''
+
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)){
+        mensagem = 'O CPF digitado não é válido.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf){
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999',
+    ]
+   let cpfValido = true
+
+   valoresRepetidos.forEach(valor => {
+    if(valor == cpf){
+        cpfValido = false
+    }
+   })
+   return cpfValido
+}
+
+function checaEstruturaCPF(cpf){
+    const multiplicador  = 10
+
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function checaDigitoVerificador(cpf, multiplicador){
+    if(multiplicador >= 12){
+        return true
+    }
+
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
+    const digitoVerificador =  cpf.charAt(multiplicador-1)
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--){
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)){
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false
+}
+
+function confirmaDigito(soma){
+    return 11 - (soma % 11)
+}
